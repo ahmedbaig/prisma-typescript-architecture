8b1432df27c63a1a9ea70c711a2a089cdd1c2735
Ahmed Baig <ahmedbaig54@yahoo.com>
2021-02-18 20:12:10 +0500

    appRoot fix

--- app/http/controller/api/web/error/error.controller.ts
+++ app/http/controller/api/web/error/error.controller.ts
@@ -1,15 +1,12 @@
 import path from "path";
-
-const globalAny: any = global;
-globalAny.ROOTPATH = __dirname;
-
+import * as appRoot from 'app-root-path'
 export let not_found_page = async function (req, res) {
-    res.render(path.join(globalAny.ROOTPATH, "views/views/error/404.ejs"));
+    res.render(path.join(appRoot.path, "views/error/404.ejs"));
 };
 
 export let internal_server_error = async function (req, res) {
     if (req.query.err == null || req.query.err == "") {
         req.query.err = "Misuse of resource";
     }
-    res.render(path.join(globalAny.ROOTPATH, "views/views/error/500.ejs"), { error: req.query.err });
+    res.render(path.join(appRoot.path, "views/views/error/500.ejs"), { error: req.query.err });
 };
